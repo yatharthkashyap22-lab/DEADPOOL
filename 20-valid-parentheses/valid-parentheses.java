1f2dc2d84c23152df854a2d52ac953d1e6e07608
yatharthkashyap22-lab <yatharthkashyap22@gmail.com>
2026-04-07 11:21:35 +0530

    Time: 4 ms (41.18%) | Memory: 43.4 MB (47.64%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            } else {
+                if(st.isEmpty()) return false;
+
+                if(ch == ')' && st.peek() != '(') return false;
+                if(ch == '}' && st.peek() != '{') return false;
+                if(ch == ']' && st.peek() != '[') return false;
+
+                st.pop();
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
