b701d739106e775c2cd41e9f07353e373e55a747
yatharthkashyap22-lab <yatharthkashyap22@gmail.com>
2026-04-14 11:19:57 +0530

    Time: 9 ms (27.07%) | Memory: 47.2 MB (9.41%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,18 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            int count = 0;
-            
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            
-            if(count == 1){
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length-1; i+=2){
+            if(nums[i] != nums[i + 1]){
                 return nums[i];
             }
         }
-        return -1;
+
+        return nums[nums.length - 1];
     }
 }
