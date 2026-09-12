ce6fe9f58ce852ef04201291c290d651cbf66991
yatharthkashyap22-lab <yatharthkashyap22@gmail.com>
2026-09-12 20:25:38 +0530

    Time: 55 ms (5.03%) | Memory: 42.6 MB (61.35%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        int ans = 0;
+        for(int i = 1; i <= x; i++) {
+            if(i <= x / i) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+}
