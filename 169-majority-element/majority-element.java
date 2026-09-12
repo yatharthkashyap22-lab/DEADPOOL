e999ed99e36dec41bd464cdd55cc571f18d2c1b7
yatharthkashyap22-lab <yatharthkashyap22@gmail.com>
2026-09-12 20:35:44 +0530

    Time: 2 ms (70.28%) | Memory: 63.2 MB (6.67%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(count == 0) {
+                ans = nums[i];
+            }
+            if(nums[i] == ans) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return ans;
+    }
+}
