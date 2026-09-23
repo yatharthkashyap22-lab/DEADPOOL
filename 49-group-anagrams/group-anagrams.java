ffcdc1bd95fa175cf7f5e73510a0ba7e523a9093
yatharthkashyap22-lab <yatharthkashyap22@gmail.com>
2026-09-23 12:14:03 +0530

    Time: 7 ms (72.21%) | Memory: 49.8 MB (34.79%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+
+            char[] chars = strs[i].toCharArray();
+
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(strs[i]);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
